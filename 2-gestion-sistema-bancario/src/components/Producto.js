87bb8d57443d8c2e9f09f1b0e7545c4d552a13db
Jhonsking49 <juanreygonzalez2002@gmail.com>
2024-11-29 09:34:55 +0100

    Product class created

--- 2-gestion-sistema-bancario/src/components/Producto.js
+++ 2-gestion-sistema-bancario/src/components/Producto.js
@@ -1,11 +1,24 @@
 
-export function producto (nombre, precio, stock, imagen) {
+export function producto (id, nombre, precio, stock, imagen) {
+    this.id = Math.random();
     this.nombre = nombre;
     this.precio = precio;
     this.stock = stock;
     this.imagen = imagen;
 
     this.obtenerInformacion = function() {
-        return 
+        return `Los datos del producto son: Nombre: ${this.nombre}, Precio: ${this.precio}, Stock: ${this.stock}, Imagen: ${this.imagen}`;
     }
+
+    this.cambiarStock = function(newStock) {
+        if(newStock > stock) {
+            this.stock = stock + newStock;
+        } else if (newStock < stock) {
+            this.stock = stock - newStock;
+        } else {
+            this.stock = stock;
+            return "No se puede cambiar el stock";
+        }
+        return `El nuevo stock es: ${this.stock}`;
+    } 
 }
---
+++ 2-gestion-sistema-bancario/src/main.js
@@ -0,0 +1,47 @@
+import { producto } from "./components/Producto.js";
+
+const productos = [
+    {
+        id: 1,
+        nombre: "iPhone 12",
+        precio: 1000,
+        stock: 10,
+        imagen: "https://www.apple.com/v/iphone/home/ah/images/overview/feature_iphone_12.png"
+    },
+    {
+        id: 2,
+        nombre: "iPhone 12 Pro",
+        precio: 900,
+        stock: 10,
+        imagen: "https://www.apple.com/v/iphone/home/ah/images/overview/feature_iphone_12_pro.png"
+    },
+    {
+        id: 3,
+        nombre: "iPhone 12 Pro Max",
+        precio: 800,
+        stock: 10,
+        imagen: "https://www.apple.com/v/iphone/home/ah/images/overview/feature_iphone_12_pro_max.png"
+    },
+    {
+        id: 4,
+        nombre: "Xiaomi Redmi Note 11",
+        precio: 200,
+        stock: 50,
+        imagen: "https://www.apple.com/v/iphone/home/ah/images/overview/feature_iphone_12.png"
+    },
+    {
+        id: 5,
+        nombre: "Xiaomi Redmi Note 11 Pro",
+        precio: 300,
+        stock: 30,
+        imagen: "https://www.apple.com/v/iphone/home/ah/images/overview/feature_iphone_12_pro.png"
+    },
+    {
+        id: 6,
+        nombre: "Xiaomi Redmi Note 11 Pro Max",
+        precio: 400,
+        stock: 20,
+        imagen: "https://www.apple.com/v/iphone/home/ah/images/overview/feature_iphone_12_pro_max.png"
+    }
+]
+
